b52fefef978c70eefd340395fec076c4a899aa7f
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-06-22 08:52:43 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '08b4c33347a82d4da1cd1809453c575d81da9d27f9c3b9fb0a122d781b78ec383ef3cbf8afab8cb4a2f4f6dcbafb7031b6daa811e714d635c79d6a41084724c3'
